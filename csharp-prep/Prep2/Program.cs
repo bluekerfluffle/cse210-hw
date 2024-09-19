b9ede695e74fae6fabf83424ce793d28693ee45d
bluekerfluffle <gdunn3@byui.edu>
2024-09-18 19:47:40 -0600

    competed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,77 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string userpercent = Console.ReadLine();
+        int percent = int.Parse(userpercent);
+        string letter;
+
+
+        if (percent >= 90)
+        {
+            letter = "A";
+
+            if (percent < 93)
+            {
+                letter = "A-";
+            }
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+
+            if (percent >=87)
+            {
+                letter = "B+";
+            }
+
+            else if (percent < 83)
+            {
+                letter = "B-";
+            }
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+
+            if (percent >=77)
+            {
+                letter = "C+";
+            }
+
+            else if (percent < 73)
+            {
+                letter = "C-";
+            }
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+
+            if (percent >=67)
+            {
+                letter = "D+";
+            }
+
+            else if (percent < 63)
+            {
+                letter = "D-";
+            }
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed");
+        }
+        else if (percent <70)
+        {
+            Console.WriteLine("Try Again Next Time");
+        }
+
+        Console.WriteLine($"Your letter grade is an {letter}");
     }
 }
