b93a96f9692aa3ec46b20e01a3264331b51ff87d
bluekerfluffle <gdunn3@byui.edu>
2024-09-23 16:53:00 -0600

    started prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is the magic number?");
+
+        string userMagic = Console.ReadLine();
+        int magic = int.Parse(userMagic);
+
+        Console.Write("What is your guess?");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse(userGuess);
+
+        while (guess != magic)
+        {
+            if (guess >= magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess <= magic)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        
+        if (guess == magic)
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
