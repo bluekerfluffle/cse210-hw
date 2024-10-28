b24c4b86cff9699a54207bb26258ef9f61efca7a
bluekerfluffle <gdunn3@byui.edu>
2024-10-28 15:34:16 -0600

    completed learning activity

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -1,19 +1,22 @@
 using System;
-
 public class Assignment{
     private string _studentName;
     private string _topic;
 
-    public Assignment(string _studentName, string _topic){
+    public Assignment(string studentName, string topic){
         _studentName = studentName;
         _topic = topic;
 
     }
 
-    public GetStudentName{
-
+    public string GetStudentName(){
+        return _studentName;
     }
-    public Get
-
+    public string GetTopic(){
+        return _topic;
+    }
+    public string GetSummary(){
+        return _studentName + " - " + _topic;
+    }
 
 }
--- prepare/Learning04/WritingAssignment.cs
+++ prepare/Learning04/WritingAssignment.cs
@@ -1,5 +1,15 @@
 using System;
 
-class WritingAssignment{
-    string _title;
+class WritingAssignment : Assignment {
+    private string _title;
+
+    public WritingAssignment (string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+    public string GetWritingInformation(){
+        string studentName = GetStudentName();
+        return $"{_title} by {studentName}";
+    }
 }
